bf2b64027746e1ae823059cf8fd6b51431e66f1f
root <root@ed499d7e491c.mylabserver.com>
2020-12-20 12:45:37 +0000

    added hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,7 @@
 # @summary A short summary of the purpose of this class
 #
-class apache::install (
-  $include_name   = $apache::params::install_name,
-  $include_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package { "${install_name}":
-   ensure => $install_ensure,
+class apache::install {
+  package { "${apache::install_name}":
+   ensure => $apache::install_ensure,
   }
 }
