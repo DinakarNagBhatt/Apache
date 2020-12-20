3b259f16fbfa19a688b539597432e2de3c746a0c
root <root@ed499d7e491c.mylabserver.com>
2020-12-20 11:27:15 +0000

    updated fields in param

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,10 @@
 # @summary A short summary of the purpose of this class
 #
-class apache::install {
-  package { 'httpd':
-   ensure => present,
+class apache::install (
+  $include_name   = $apache::params::install_name,
+  $include_ensure = $apache::params::install_ensure,
+) inherits apache::params {
+  package { "${install_name}":
+   ensure => $install_ensure,
   }
 }
